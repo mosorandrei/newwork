84efa788de59351a5ec7425762663d36064c8712
DESKTOP-5O8CUVJ\ROG <mosorandrei49@gmail.com>
2025-10-07 19:47:51 +0300

    initial setup

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+        <div className="min-h-screen grid place-items-center">
+            <div className="text-xl font-bold text-primary">
+                Tailwind is working
+            </div>
+        </div>
+    );
+}
